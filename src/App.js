0abd3f60eb4877a23d087a0c40e0aca5afc997ce
Joseph M <jmedina1985@hotmail.com>
2021-01-26 21:38:31 -0800

    Still getting set up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import GlobalStyles from './components/GlobalStyles';
+
 function App() {
   return (
     <div className="App">
+      <GlobalStyles />
       <h1>A pink modal will live here</h1>
     </div>
   );
---
+++ src/components/GlobalStyles.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: 'Space Grotesk', sans-serif;
+}
+
+h1 {
+  font-family: 'Space Grotesk', sans-serif;
+}
+`;
+
+export default GlobalStyles;
