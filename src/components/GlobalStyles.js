9cd54340bec1aa94bf408695e94c7dc24f543887
Joseph M <jmedina1985@hotmail.com>
2021-01-27 23:47:56 -0800

    Modal - 5

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -2,39 +2,17 @@ import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
 * {
+  box-sizing: border-box;
   margin: 0;
   padding: 0;
-  box-sizing: border-box;
+  font-family: 'Space Grotesk', sans-serif;
 }
 
 body {
-  font-family: 'Space Grotesk', sans-serif;
   background-color: #fcfcfc;
 }
 
-h1 {
-  font-family: 'Space Grotesk', sans-serif;
-  color: #946a6a;
-  font-size: 3rem;
-}
 
-h2 {
-  font-family: 'Space Grotesk', sans-serif;
-  color: #d6b0b0;
-  font-size: 2.5rem;
-}
-
-h3 {
-  font-family: 'Space Grotesk', sans-serif;
-  color: #cfb753;
-  font-size: 3rem;
-}
-
-h4 {
-  font-family: 'Space Grotesk', sans-serif;
-  color: #fcfcfc;
-  font-size: 2.5rem;
-}
 
 `;
 
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const Background = styled.div`
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.8);
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 export const Modal = ({ showModal, setShowModal }) => {
   return <>{showModal ? <div>Modal</div> : null}</>;
 };
