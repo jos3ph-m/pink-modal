7d36504ead98e09622ff2af0e05b571f33337ea0
Joseph M <jmedina1985@hotmail.com>
2021-01-28 23:46:41 -0800

    Modal - 13

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -17,7 +17,7 @@ const ModalWrapper = styled.div`
   width: 800px;
   height: 500px;
   box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
-  background: #fff;
+  background: #f7d9cf;
   color: #000;
   display: grid;
   grid-template-columns: 1fr 1fr;
