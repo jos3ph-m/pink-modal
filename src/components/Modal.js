eb625f9a1d004bbd07803fb897e9e3cc7c049a29
Joseph M <jmedina1985@hotmail.com>
2021-01-30 13:31:30 -0800

    Functionality works correctly

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -81,10 +81,30 @@ export const Modal = ({ showModal, setShowModal }) => {
     transform: showModal ? `translateY(0%)` : `translateY(-100%)`,
   });
 
+  const closeModal = (e) => {
+    if (modalRef.current === e.target) {
+      setShowModal(false);
+    }
+  };
+
+  const keyPress = useCallback(
+    (e) => {
+      if (e.key === 'Escape' && showModal) {
+        setShowModal(false);
+      }
+    },
+    [setShowModal, showModal]
+  );
+
+  useEffect(() => {
+    document.addEventListener('keydown', keyPress);
+    return () => document.removeEventListener('keydown', keyPress);
+  }, [keyPress]);
+
   return (
     <>
       {showModal ? (
-        <Background ref={modalRef}>
+        <Background ref={modalRef} onClick={closeModal}>
           <animated.div style={animation}>
             <ModalWrapper showModal={showModal}>
               <ModalImg src={modal} alt="desert" />
